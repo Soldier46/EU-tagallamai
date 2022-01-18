65ee9a139fa20b2bdb7cea44840a8db75d225842
Soldier46 <rschuszler46@gmail.com>
2022-01-18 10:21:34 +0100

    Add project files.

---
+++ EU-tagallamai/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Dynamic;
+
+
+
+
+
+
+using System;
+
+namespace EU_tagallamai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
